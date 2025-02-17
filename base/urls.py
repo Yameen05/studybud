731c44e7c576489c68c67c044cfbb153446d4cd4
Yameen05 <103006919+Yameen05@users.noreply.github.com>
2025-02-16 23:31:08 -0500

    new FEB16

--- base/urls.py
+++ base/urls.py
@@ -4,11 +4,18 @@ from . import views
 urlpatterns = [
 
     path('login/', views.loginPage, name='login'),  # Login page
+    path('logout/', views.logoutUser, name='logout'),  # Login page
+    path('register/', views.registerPage, name='register'),
+
+    
 
     path('', views.home, name='home'),  # Home page
     path('room/<str:pk>/', views.room, name='room'),  # Room page
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),  # User profile page
 
     path('create-room/', views.createRoom, name='create-room'),  # Create room page
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),  # Update room page
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),  # Delete room page
+    path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),  # Delete room page
+
 ]
